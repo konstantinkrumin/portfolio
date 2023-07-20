e7047e1a967452181df04c4e98ce16b4ecb89440
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-07-20 00:02:38 -0400

    configured eslint and prettier for the project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,10 +4,13 @@ module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
 	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-		'plugin:react-hooks/recommended'
+		"eslint:recommended",
+    	"plugin:react/recommended",
+    	"plugin:react-hooks/recommended",
+    	"plugin:import/recommended",
+    	"plugin:jsx-a11y/recommended",
+    	"plugin:@typescript-eslint/recommended",
+    	"eslint-config-prettier"
 	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
