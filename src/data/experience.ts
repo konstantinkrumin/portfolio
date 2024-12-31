39c8e29fa806e8ac64f1cac251b0329dd6da8930
Konstantin Krumin <konstantin.krumin@gmail.com>
2024-12-30 21:50:51 -0500

    Create a portfolio app

---
+++ src/data/experience.ts
@@ -0,0 +1,61 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "2023 — Present",
+    title: "Software Developer / Team Lead",
+    company: "BorderPass",
+    companyUrl: "https://www.borderpass.ca/",
+    description:
+      "BorderPass is a fast-growing legal tech startup dedicated to simplifying the visa journey for international students in Canada through innovative technology solutions.",
+    technologies: [
+      "Typescript",
+      "React",
+      "Node.js",
+      "Next.js",
+      "GraphQL",
+      "Sequelize",
+      "PostgreSQL",
+      "Jest",
+      "AWS",
+    ],
+    logo: "/logos/borderpass.jpg",
+  },
+  {
+    dates: "2021 - 2023",
+    title: "Full Stack Developer",
+    company: "Hölmetrics",
+    companyUrl: "https://holmetrics.com/",
+    description:
+      "Hölmetrics is a data-analytics company that provides enterprise-level employee experience and organizational performance analysis. The company enables leaders to make real-time data-informed decisions, driving both employee satisfaction and business performance.",
+    technologies: [
+      "Typescript",
+      "Javascript",
+      "React",
+      "Redux",
+      "Node.js",
+      "Express.js",
+      "TailwindCSS",
+      "MySQL",
+      "AWS",
+    ],
+    logo: "/logos/holmetrics.jpg",
+  },
+  {
+    dates: "2020 - 2021",
+    title: "Frontend Developer",
+    company: "ICL Services",
+    companyUrl: "https://icl-services.com/eng/",
+    description:
+      "ICL Services is a leading project-based outsourcing company in Russia, providing IT solutions to a diverse range of industries. Known for its expertise and innovation, the company serves as a trusted partner to businesses across the region.",
+    technologies: [
+      "Typescript",
+      "Javascript",
+      "React",
+      "Redux",
+      "MobX",
+      "Material UI",
+    ],
+    logo: "/logos/icl_services.jpg",
+  },
+];
